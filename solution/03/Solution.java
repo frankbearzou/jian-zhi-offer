91ba96c4181981efd20489d4db5c9855413b316e
Frank Zou <frankbearzou@gmail.com>
2022-09-18 12:37:26 -0400

    Create Solution.java

---
+++ solution/03/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
