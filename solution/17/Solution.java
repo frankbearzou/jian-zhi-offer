16b69fd2b05ffe6a5b398f92fe1c7b9c70a75141
Frank Zou <frankbearzou@gmail.com>
2022-10-08 15:55:06 -0400

    Create Solution.java

---
+++ solution/17/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] printNumbers(int n) {
+        int size = 1;
+        for (int i = 0; i < n; i++) {
+            size = size * 10;
+        }
+        // n = 1, size = 9
+        // n = 2, size = 99
+        // n = 3, size = 999
+        size--;
+        int[] res = new int[size];
+        for (int i = 0; i < size; i++) {
+            res [i] = i + 1;
+        }
+        return res;
+    }
+}
