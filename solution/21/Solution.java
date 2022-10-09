67b83a33bab90f7f2ddcc6bf07b4beec5d2123a5
Frank Zou <frankbearzou@gmail.com>
2022-10-09 17:03:59 -0400

    Create Solution.java

---
+++ solution/21/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] exchange(int[] nums) {
+        int i = 0, j = nums.length - 1, temp;
+        while (i < j) {
+            while (i < j && (nums[i] & 1) == 1) {
+                i++;
+            }
+            while (i < j && (nums[j] & 1) == 0) {
+                j--;
+            }
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        return nums;
+    }
+}
