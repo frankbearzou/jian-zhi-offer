d769d49d6a408d1e6cadf281d881e4b4619b2823
Frank Zou <frankbearzou@gmail.com>
2022-10-09 14:28:00 -0400

    Create Solution.java

---
+++ solution/18/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode pre = new ListNode(-1);
+        ListNode cur = new ListNode(-1);
+
+        cur.next = head;
+        pre = cur;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+                break;
+            }
+            cur = cur.next;
+        }
+        return pre.next;
+    }
+}
