641caf33fa546fcf10b5f7f25788629c021b31c7
Frank Zou <frankbearzou@gmail.com>
2022-11-01 19:43:01 -0400

    Create Solution.java

---
+++ solution/28/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSame(root.left, root.right);
+    }
+
+    boolean isSame(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return isSame(node1.left, node2.right) && isSame(node1.right, node2.left);
+    }
+}
